f5495f60ab32623a26db000c1e157c548c244861
Sashankr <sashankr97@gmail.com>
2023-05-28 16:26:55 +0530

    added login page ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,23 @@
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2 className="text-3xl mt-5 mb-5 ">Login</h2>
+      <form action="">
+        <Input
+          type="email"
+          placeholder="Ex: rohit@gmail.com"
+          label="Email"
+          name="email"
+        />
+        <Input type="password" label="Password" name="password" />
+
+        <Button title="Login" type="secondary" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
