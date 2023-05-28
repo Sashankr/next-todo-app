227d236e3a297e93ef5f9b1298c50947e6c0b71e
Sashankr <sashankr97@gmail.com>
2023-05-28 15:21:51 +0530

    added metadata for pages

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
+const Dashboard = () => {
+  return <div>Hello, Dashboard Page!</div>;
+};
+
+export default Dashboard;
